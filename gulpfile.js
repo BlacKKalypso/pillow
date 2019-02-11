71ee486e7cdfcfd491ddffce7521201cd4e1e7e2
BlacKKalypso <nancy.tonye@crea-inseec.com>
2019-02-11 15:12:08 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var $ = require('gulp-load-plugins')({lazy: true});
+var browserSync = require('browser-sync').create();
+var git = require('gulp-git');
+
+gulp.task('scss', function () {
+    console.log('Run scss tasks');
+
+    return gulp
+        .src('assets/sass/*.scss')
+        .pipe($.sass({outputStyle: 'compressed'}))
+        .pipe(rename('style.css'))
+        .pipe(gulp.dest('assets/css/'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('assets/sass/*.scss', gulp.series('scss'));
+});
+
+gulp.task('init', function(){
+    git.init(function (err) {
+        if (err) throw err;
+    });
+});
+
+gulp.task('commit', function(){
+    return gulp.src('./git-test/*')
+        .pipe(git.commit('initial commit'));
+});
+ 
